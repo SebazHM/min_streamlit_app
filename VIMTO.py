52ee7665ee4bd610d0701484d4b3b5a162070959
SebazHM <sebstian.ozog@elev.ga.ntig.se>
2024-09-18 12:44:35 +0200

    Update VIMTO.py

--- VIMTO.py
+++ VIMTO.py
@@ -1,1 +1,17 @@
+import datetime
 
+import streamlit as st
+
+st.title("Min första Streamlit-app")
+st.header("Välkommen!")
+st.subheader("Här lär vi oss Streamlit")
+
+if st.button("Klicka här!"):
+    st.write("Du klickade på knappen!")
+
+namn = st.text_input("Vad heter du?")
+if namn:
+    st.write(f"Hej, {namn}! Välkommen till Stremlit.")
+
+print (datetime.date.today())
+st.write(datetime.date.today())
