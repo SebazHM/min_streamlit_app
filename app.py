637839a9e2afa5e65484248414a1c4b5309a57f8
SebazHM <sebstian.ozog@elev.ga.ntig.se>
2024-10-02 12:36:31 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import datetime
+table = dynamodb.Table("inlägg")
+today = datetime.datetime.today()
+week_num = today.isocalendar()[1]
+st.write ("Vecka:",week_num,"")
+date = st.date_input ("Vecka")
+txt = st.text_area('')
+st.feedback("stars")
+st.button ("Spara")
+st.button ("Redigera")
+
+def add_entry(week_num,date,txt):
+ table.put_item(
+Item={
+"week_num":week_num,
+"date":date,
+"txt":txt,
+}
+)
