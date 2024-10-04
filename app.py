b5ac25b8735936c1df383fec393d750866354b52
SebazHM <sebstian.ozog@elev.ga.ntig.se>
2024-10-04 11:19:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import datetime
 import boto3
+
+def add_entry(week_num,date,txt):
+ table.put_item(
+Item={
+"week_num":week_num,
+"date":date,
+"txt":txt,
+}
+)
 AWS_REGION = "us-east-1"  # Ändra detta till din önskade region, t.ex. "eu-west-1" för Irland
 # Skapa en DynamoDB-resurs med specificerad region
 dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
@@ -11,14 +20,10 @@ st.write ("Vecka:",week_num,"")
 date = st.date_input ("Vecka")
 txt = st.text_area('')
 st.feedback("stars")
-st.button ("Spara")
+if st.button ("Spara"):
+ add_entry(week_num,date,txt)
+ 
 st.button ("Redigera")
 
-def add_entry(week_num,date,txt):
- table.put_item(
-Item={
-"week_num":week_num,
-"date":date,
-"txt":txt,
-}
-)
+
+
