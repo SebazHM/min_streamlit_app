35a0c5126563d46b890e9484c578843ba6bbfca9
SebazHM <sebstian.ozog@elev.ga.ntig.se>
2024-10-04 11:02:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+AWS_REGION = "us-east-1"  # Ändra detta till din önskade region, t.ex. "eu-west-1" för Irland
+# Skapa en DynamoDB-resurs med specificerad region
+dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
 import streamlit as st
 import datetime
 table = dynamodb.Table("MinBas")
